bb1fbcd21bf6d62982a221b63f38aa69f5711869
陆轶杰 <1476408542@qq.com>
2022-05-07 18:23:15 +0800

    今天更新捏

--- python1/main.py
+++ python1/main.py
@@ -16,6 +16,7 @@ def print_bye(name):
 if __name__ == '__main__':
     print_hi('lu')
     print_bye('lu')
+    print('caoxiangqian')
 #给你更新个东西,再更新个东西
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
